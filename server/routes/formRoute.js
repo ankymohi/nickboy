887d918b114b454754bc0f64750b8caf944f2c54
aniket kumar <118229208+ankymohi@users.noreply.github.com>
2025-11-27 02:01:26 +0530

    Update formRoute.js

--- server/routes/formRoute.js
+++ server/routes/formRoute.js
@@ -26,15 +26,16 @@ router.post(
      const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // IMPORTANT
+  secure: false,
   auth: {
-    user: "ak8628041311@gmail.com",
-    pass: "qitf gajs mbrl incv",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
   tls: {
-    rejectUnauthorized: false, // BYPASS self-signed certificate
+    rejectUnauthorized: false, 
   },
 });
+
 
 
       // Verify SMTP connection
